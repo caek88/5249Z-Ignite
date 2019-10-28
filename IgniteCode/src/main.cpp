e66a1739567a99092f6c320c2cf61e42e2f7aa2c
caek88 <35225849+caek88@users.noreply.github.com>
2019-10-28 17:51:00 -0400

    VexCode initial setup
    Created the project file on VexCode

---
+++ IgniteCode/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Cameron                                          */
+/*    Created:      Mon Oct 28 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
