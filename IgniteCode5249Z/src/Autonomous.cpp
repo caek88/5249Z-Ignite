53b11e969e092ed208d29391cb5667c0b62016b7
caek88 <35225849+caek88@users.noreply.github.com>
2019-11-20 16:57:19 -0500

    Tested PID

--- IgniteCode5249Z/src/Autonomous.cpp
+++ IgniteCode5249Z/src/Autonomous.cpp
@@ -1,11 +1,22 @@
 #include "RobotConfig.h"
 #include "PID.h"
 int auton(){
-    PID driveForward = PID(1, 0, 0, 0.01);
-    driveForward.setPoint = 100;
+    PID driveForward = PID(10.0/7.0, 0, 4.0/42.0, 0.01);
+    driveForward.setPoint = 1000;
     while(true){
         double speed = driveForward.calculatePID(mtrLeft.rotation(rotationUnits::deg));
+        if (speed > 80){
+            speed = 80;
+        }
+        if (speed < -80){
+            speed = -80;
+        }
         mtrLeft.spin(directionType::fwd, speed, velocityUnits::pct);
+        mtrRight.spin(directionType::fwd, speed, velocityUnits::pct);
+        Brain.Screen.clearScreen();
+        Brain.Screen.setCursor(1,1);
+        Brain.Screen.print("%f", mtrLeft.rotation(rotationUnits::deg));
+        task::sleep(10);
     }
     return 0;
 }
