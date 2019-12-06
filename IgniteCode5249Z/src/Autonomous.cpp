45f525193bd79c9b7898eb46de0198a216726927
caek88 <35225849+caek88@users.noreply.github.com>
2019-12-06 18:39:57 -0500

    Update Autonomous.cpp

--- IgniteCode5249Z/src/Autonomous.cpp
+++ IgniteCode5249Z/src/Autonomous.cpp
@@ -2,16 +2,11 @@
 
 int auton(){
     //deployRobot();
-    int i = 1;
-    while(true){
-      if(i == 1){
-        
-        vex::task driveTask = task(drivePID);
-        vex::task positionTask = vex::task(pos);
-        driveToPos(12.0, 0);
-      }
-      i=2;
-      task::sleep(1000);
-    }
+    vex::task driveTask = task(drivePID);
+    vex::task positionTask = vex::task(pos);
+    driveToPos(12.0, 0);
+    task::sleep(1000);
+    driveToPos(12.0, 12.0);
+    task::sleep(1000);
     return 0;
 }
