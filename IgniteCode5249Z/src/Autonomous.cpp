ce12e5a6fe4c3769e9ea65b7af559de733045172
caek88 <35225849+caek88@users.noreply.github.com>
2019-12-06 17:43:09 -0500

    Update Autonomous.cpp

--- IgniteCode5249Z/src/Autonomous.cpp
+++ IgniteCode5249Z/src/Autonomous.cpp
@@ -1,38 +1,4 @@
 #include "RobotConfig.h"
-#include "PID.h"
-double maxSpeed = 80;
-PID driveLeft = PID(10.0/7.0, 0, 4.0/42.0, 0.01);
-PID driveRight = PID(10.0/7.0, 0, 4.0/42.0, 0.01);
-int drivePID(){
-    while (true){
-        driveLeft.setPoint = leftPosition;
-        driveRight.setPoint = rightPosition;
-        double speedLeft = driveLeft.calculatePID(mtrLeft.rotation(rotationUnits::deg));
-        double speedRight = driveRight.calculatePID(mtrRight.rotation(rotationUnits::deg));
-        Brain.Screen.printAt(1, 120, true, "Position Left: %f", leftPosition);
-        Brain.Screen.printAt(1, 150, true, "Position Right: %f", rightPosition);
-        if (speedLeft > maxSpeed){
-            speedLeft = maxSpeed;
-        }
-        if (speedRight > maxSpeed){
-            speedRight = maxSpeed;
-        }
-        if (speedLeft < -maxSpeed){
-            speedLeft = -maxSpeed;
-        }
-        if (speedRight < -maxSpeed){
-            speedRight = -maxSpeed;
-        }
-        mtrLeft.spin(directionType::fwd, speedLeft, velocityUnits::pct);
-        mtrLeftFront.spin(directionType::fwd, speedLeft, velocityUnits::pct);
-        mtrRight.spin(directionType::fwd, speedRight, velocityUnits::pct);
-        mtrRightFront.spin(directionType::fwd, speedRight, velocityUnits::pct);
-        
-        task::sleep(10);
-    }
-    return 0;
-}
-
 
 int auton(){
     //deployRobot();
